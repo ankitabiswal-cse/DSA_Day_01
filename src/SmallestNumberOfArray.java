ee975009ea89e07ea2535fb7d45f8affd821e3ec
Ankita Biswal <ankitabiswal0113@gmail.com>
2026-09-23 19:26:05 +0530

    DSA Day 01:Array Basics

--- src/SmallestNumberOfArray.java
+++ src/SmallestNumberOfArray.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
-
 public class SmallestNumberOfArray {
+    public static void main(String[] args){
+        int[] arr = {29,45,36,3,45,67,1,90,0,89};
+        int smallest = arr[0];
+
+        for(int i = 0;i<arr.length;i++){
+            if(arr[i]<smallest){
+                smallest = arr[i];
+            }
+        }
+        System.out.println("The Smallest Number Of Array ="+smallest);
+    }
 }
